cb95d8fd948023a7aadefce495d9ea1c8b63b516
Oldrick Weenink <o.weenink@hotmail.com>
2023-12-17 12:11:42 +0100

    Added simple Database connection

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 
 require "functions.php";
+//require "router.php";
 
-require "router.php";
+$dsn = "mysql:host=localhost;port=3306;user=root;password=password;charset=utf8mb4;dbname=demo";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
+
+
+echo "<ul>";
+foreach ($posts as $post) {
+    echo "<li>$post[title]</li>";
+}
+echo "</ul>";
--- router.php
+++ router.php
@@ -2,27 +2,27 @@
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 
 $routes = [
-'/' => 'controllers/index.php',
-'/contact' => 'controllers/contact.php',
-'/about' => 'controllers/about.php'
+    '/' => 'controllers/index.php',
+    '/contact' => 'controllers/contact.php',
+    '/about' => 'controllers/about.php'
 ];
 
 
 function abort($code)
 {
-http_response_code($code);
-require "views/{$code}.php";
-die();
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
 }
 
 
 function routeToController($uri, $routes)
 {
-if (array_key_exists($uri, $routes)) {
-require $routes[$uri];
-} else {
-abort(404);
-}
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort(404);
+    }
 }
 
 routeToController($uri, $routes);
